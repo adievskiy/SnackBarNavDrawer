800e2fc6d0e98cd7fb60e508a1990d4f79166371
adievskiy <dievskiy@mail.ru>
2024-12-20 23:20:39 +0400

    complete

--- app/src/main/java/com/example/snackbarnavdrawer/MainActivity.kt
+++ app/src/main/java/com/example/snackbarnavdrawer/MainActivity.kt
@@ -1,14 +1,11 @@
 package com.example.snackbarnavdrawer
 
 import android.annotation.SuppressLint
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -16,7 +13,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
@@ -48,7 +44,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -154,6 +149,7 @@ fun MainScreen(notes: List<Note>, onDeleteNote: (Note) -> Unit, onNavigateToAddN
                         }
                     }
                 },
+                scrimColor = Color.DarkGray,
                 content = {
                     Column(Modifier.fillMaxSize()) {
                         IconButton(onClick = {
@@ -176,8 +172,7 @@ fun MainScreen(notes: List<Note>, onDeleteNote: (Note) -> Unit, onNavigateToAddN
                             modifier = Modifier.fillMaxWidth()
                         )
                     }
-                },
-                scrimColor = Color.DarkGray
+                }
             )
         }
     }
